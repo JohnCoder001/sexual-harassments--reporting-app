cd3864d6a16577a00993d5588078665ded8ddb4e
Veronicah <njugunaveraiconicah@gmail.com>
2023-01-16 11:31:57 +0300

    first commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -17,8 +17,14 @@
     <div>
         <h1 class="font-bold text-5xl text-black">What we do</h1>
         <br>
-        <p class="text-xl text-gray-800"><a class="font-bold text-3xl text-gray-800">H</a>ere we assist victims or relatives to victims report cases and get help.We help you get assistance from professionals,for example,psychologists who will organize a check-up for you,lawyers who will help in fighting for your case in case reported,people who have experienced such cases and will encourage you to not do offensive things like suicide.</p>
+        <p class="text-xl text-gray-800"><a class="font-bold text-3xl text-gray-800">H</a>ere we assist
+            victims or relatives to victims report cases and get help.We help you get assistance from
+            professionals,for example,psychologists who will organize a check-up for you,lawyers who will
+            help in fighting for your case in case reported,people who have experienced such cases and
+            will encourage you to not do offensive things like suicide.</p>
         <br>
-        <p class="text-xl text-gray-800">Another good thing with us is that your information is safe ,the information is between you and us other victims who come to lay their cases are not able to see or acces your information.</p>
+        <p class="text-xl text-gray-800">Another good thing with us is that your information is safe
+             ,the information is between you and us, other victims who come to lay their cases are not
+             able to see or acces your information.</p>
     </div>
 </x-app-layout>
